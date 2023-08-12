317e574a9fa103c6c98ed004daf1f0f217ccf53b
nata2806 <vysochina_natalja@ukr.net>
2023-08-12 13:07:40 +0300

    11

--- src/js/pixabay.js
+++ src/js/pixabay.js
@@ -1,22 +1,21 @@
 import axios from "axios";
 
-const BAZE_URL = 'https://pixabay.com/api/';
+const BAZE_URL = 'https://pixabay.com/api/'
 
+const getPhotosService = async (value, page) => {
 
-async function getPhotosService(value, page) {
-
-  const { data } = await axios(BAZE_URL, {
-    params: {
-      key: '38700636-ea33df46ee2c91044811eb016',
-      q: `${value}`,
-      image_type: "photo",
-      orientation: "horizontal",
-      safesearch: "true",
-      page: page,
-      per_page: 40,
-    }
-  });
-  return data;
+    const { data } = await axios(BAZE_URL, {
+        params: {
+            key: '38700636-ea33df46ee2c91044811eb016',
+            q: `${value}`,
+            image_type: "photo",
+            orientation: "horizontal",
+            safesearch: "true",
+            page: page,
+            per_page: 40,
+        }
+    });
+    return data;
 }
 
 export { getPhotosService };
